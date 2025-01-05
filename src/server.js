7f78c51ea4099554f6fc72e50607f31a3e004e9d
waffentracher <waffenerd@gmail.com>
2025-01-06 01:39:11 +0200

    end

--- src/server.js
+++ src/server.js
@@ -6,13 +6,23 @@ import authRouter from './routes/auth.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
-
+import YAML from 'yamljs';
+import path from 'path';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
 
 dotenv.config();
 
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 function setupServer() {
   const app = express();
+  const swaggerDocument = YAML.load(
+    path.join(__dirname, '../docs/openapi.yaml'),
+  );
+
   app.use(cookieParser());
   app.use(express.json());
   app.use(cors());
@@ -20,6 +30,7 @@ function setupServer() {
 
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use(notFoundHandler);
   app.use(errorHandler);
