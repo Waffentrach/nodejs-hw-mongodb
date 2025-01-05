4c6d80fc5bccfcd1fab4a144574dbb44287fea01
waffentracher <waffenerd@gmail.com>
2025-01-06 01:54:19 +0200

    Update cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,7 @@ export const uploadPhoto = async (filePath) => {
     });
     return result.secure_url;
   } catch (error) {
+    console.error('Cloudinary upload error:', error);
     throw new Error('Failed to upload image to Cloudinary');
   }
 };
-export { cloudinary };
